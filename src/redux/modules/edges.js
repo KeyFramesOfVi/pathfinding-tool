9f672fd4271ca2c811e117448018bd07a69eeee8
Victor Cabrera <vcabre4@gmail.com>
2018-02-15 12:34:20 -0500

    update

--- src/redux/modules/edges.js
+++ src/redux/modules/edges.js
@@ -2,7 +2,7 @@ import { createEdges } from '../../Utils/graph';
 
 export default (state = [], action) => {
   if (action.type === 'CREATE_EDGES') {
-    return createEdges(action.nodes);
+    return [...state, createEdges(action.nodes)];
   } else if (action.type === 'CREATE_PATH') {
     return action.edges;
   }
--- src/redux/modules/nodes.js
+++ src/redux/modules/nodes.js
@@ -2,9 +2,9 @@ import { createNodes, populateNodeEdges } from '../../Utils/graph';
 
 export default (state = [], action) => {
   if (action.type === 'CREATE_NODES') {
-    return createNodes(action.length, action.height, action.proximity, action.walls);
+    return [...state, createNodes(action.length, action.height, action.proximity, action.walls)];
   } else if (action.type === 'POPULATE_NODE_EDGES') {
-    return populateNodeEdges(state, action.edges);
+    return populateNodeEdges(state[0], action.edges);
   } else if (action.type === 'CREATE_PATH') {
     return action.nodes;
   }
